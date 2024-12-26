b5ef205ca4b3ad5a64b8a5d63312d8301613e43a
MarianaCifuentes <marianacifuenteslondono@gmail.com>
2024-12-26 02:48:48 -0500

    Método confirmar habitaciones

--- BookingHoteles/src/model/Reserva.java
+++ BookingHoteles/src/model/Reserva.java
@@ -1,76 +1,103 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Reserva {
     private String nombreCliente;
+    private String apellidoCliente;
     private String email;
     private String nacionalidad;
+    private String telefono;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private String tipoHabitacion;
     private int cantidadHabitaciones;
-    private String alojamientoReservado;
-    private double precioTotal;
 
-    public Reserva(String nombreCliente, String email, String nacionalidad, int cantidadHabitaciones, String alojamientoReservado, double precioTotal) {
+    public Reserva(String nombreCliente, String apellidoCliente, String email, String nacionalidad, String telefono,
+                   LocalDate fechaInicio, LocalDate fechaFin, String tipoHabitacion, int cantidadHabitaciones) {
         this.nombreCliente = nombreCliente;
+        this.apellidoCliente = apellidoCliente;
         this.email = email;
         this.nacionalidad = nacionalidad;
-        this.cantidadHabitaciones = cantidadHabitaciones;
-        this.alojamientoReservado = alojamientoReservado;
-        this.precioTotal = precioTotal;
-    }
-
-
-    // Métodos Getters y Setters
-    public String getNombreCliente() {
-        return nombreCliente;
-    }
-
-    public void setNombreCliente(String nombreCliente) {
-        this.nombreCliente = nombreCliente;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNacionalidad() {
-        return nacionalidad;
-    }
-
-    public void setNacionalidad(String nacionalidad) {
-        this.nacionalidad = nacionalidad;
-    }
-
-    public int getCantidadHabitaciones() {
-        return cantidadHabitaciones;
-    }
-
-    public void setCantidadHabitaciones(int cantidadHabitaciones) {
+        this.telefono = telefono;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.tipoHabitacion = tipoHabitacion;
         this.cantidadHabitaciones = cantidadHabitaciones;
     }
 
-    public String getAlojamientoReservado() {
-        return alojamientoReservado;
-    }
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
 
-    public void setAlojamientoReservado(String alojamientoReservado) {
-        this.alojamientoReservado = alojamientoReservado;
-    }
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
 
-    public double getPrecioTotal() {
-        return precioTotal;
-    }
+	public String getApellidoCliente() {
+		return apellidoCliente;
+	}
 
-    public void setPrecioTotal(double precioTotal) {
-        this.precioTotal = precioTotal;
-    }
-    
+	public void setApellidoCliente(String apellidoCliente) {
+		this.apellidoCliente = apellidoCliente;
+	}
 
-    public void mostrarReserva() {
-        System.out.println("Reserva realizada por: " + nombreCliente);
-        System.out.println("Alojamiento: " + alojamientoReservado + " | Habitaciones: " + cantidadHabitaciones);
-        System.out.println("Precio total: " + precioTotal);
-    }
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public String getTipoHabitacion() {
+		return tipoHabitacion;
+	}
+
+	public void setTipoHabitacion(String tipoHabitacion) {
+		this.tipoHabitacion = tipoHabitacion;
+	}
+
+	public int getCantidadHabitaciones() {
+		return cantidadHabitaciones;
+	}
+
+	public void setCantidadHabitaciones(int cantidadHabitaciones) {
+		this.cantidadHabitaciones = cantidadHabitaciones;
+	}
+
+    // Getters y Setters
+
 }
